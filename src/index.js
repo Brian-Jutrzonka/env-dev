06c75d629bde2d2df14361de46b4c9be7fc86f65
Brian Jutrzonka <bjutrzonka@dciartform.com>
2019-07-17 09:24:35 -0500

    added react and began to wire up hot reloading

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import './index.css';
-import numeral from 'numeral';
+// import { hot } from 'react-hot-loader/root';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome course!`);
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById('example'));
+
+// export default hot(App);	
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,31 +1,8 @@
 import path from 'path';
 
-
-// module.exports = {
-//   debug: true,
-//   devtool: 'inline-source-map',
-//   noInfo: false,
-//   entry: path.resolve(__dirname, 'src/index'),
-//   target: 'web',  
-//   output: {
-//     path: path.resolve(__dirname, 'src'),
-//     publicPath: '/',    
-//     filename: "bundle.js"
-//   },
-//   module: {
-//       rules: [{
-//           test: /\.scss$/,
-//           use: [
-//             "style-loader",
-//             "sass-loader"
-//           ]
-//       }]
-//   }  
-// }
-
 export default {
   debug: true,
-  devtool: 'inline-source-map',
+  devtool: 'source-map', 
   noInfo: false,
   entry: [
     path.resolve(__dirname, 'src/index')
@@ -36,11 +13,20 @@ export default {
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [],
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel',
+          query: {
+              presets: ['react', 'es2015']
+          }
+      },
+      {
+          test: /\.scss$/,
+          loaders: ['style', 'css', 'sass']
+      } 
     ]
   }
 }
